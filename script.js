df8700123ed3264fe9dec0e2b78b3c51c1344572
Faha <fakhriddintogaev29@gmail.com>
2021-12-17 22:15:46 +0500

    create script.js

---
+++ script.js
@@ -0,0 +1,544 @@
+    const quizData = [{
+            question: ' 1. In what order do managers typically perform the managerial functions?',
+            a: 'organising, planning, controlling, leading',
+            b: 'organising, leading, planning, controlling',
+            c: 'planning, organising, controlling, leading',
+            d: 'planning, organising, leading, controlling',
+            correct: 'd'
+        },
+        {
+            question: '2. At what level of an organisation does a corporate manager operate?',
+            a: 'Functional',
+            b: 'Top level',
+            c: 'Operational',
+            d: 'Middle level',
+            correct: 'b'
+        },
+        {
+            question: '3. Which one is not a recognised key skill of management?',
+            a:  'Conceptual ',
+            b:  'Human skills',
+            c:  'Writing skills',
+            d:  'Technical skills',
+            
+            correct: 'c',
+        },
+        {
+            question: '4. To what does authority refer?',
+            a:'The ability to organise people',
+            b:'The power to command and direct',	
+            c:'The need for order',
+            d:'The right to change jobs',	
+            
+            correct: 'b'
+        },
+        {
+            question: '5. What one of these is not a recognised type of authority?',
+            a:	'supplier',
+            b:'Executive',
+            c:	'functional',
+            d:	'line management',
+            
+            
+            correct: 'a'
+        },
+        {
+            question: '6. As what is ensuring that one individual does not carry the burden of a whole work task referred to?',
+            a:'	Work specialisation',
+            b:'	Work design',
+            c:'	Work schedule',
+            d:'	Work shift',
+            
+            
+            correct: ' a'
+        },
+        {
+            question: '7.What is a Gantt chart a type of?',
+            a:	'Work flow design',
+            b:	'Work rate design',
+            c:	'Work output design',
+            d:	'Work schedule design',
+            
+            
+            correct: 'd'
+        },
+        {
+            question: '8. What tasks does not Human resource management involve?',
+            a:	'Attracting employees to the organisation',
+            b:	'Recruiting employees',
+            c:'	Creating the project of the company’s building',
+            d:'	Retaining employees',
+            
+            correct: 'c'
+        },
+        {
+            question: '9.To what does diversity in the workplace refer to ',
+            a:	'The number of people employed from different ethnic groups.',
+            b:	'The range of personal characteristics of employees in an organisation.',
+            c:	'Increasing the number of women employed in the organisation.',
+            d:	'The number of different religious affiliation held by employees in the organisation.',
+            
+            
+            correct: 'b'
+        },
+           {
+            question: '10.Which of these is not part of human resource policy?',
+            a:	'Reward systems',
+            b:	'Staff appraisals',
+            c:	'Staff development',
+            d:	'Suppliers choice',
+            
+            
+            correct: 'd'
+        },
+        {
+            question: '11. Where cannot formal employee training and education take place?',
+            a:	'Colleges',
+            b:	'In the workplace',
+            c:	'Training centres',
+            d:	'In a corporate party',
+            
+            
+            correct: 'd'
+        },
+        {
+            question: '12. What are workers primarily motivated by according to the instrumental approach?',
+            a:	'Peer recognition',
+            b:	'Promotion',
+            c:	'Economic reward',
+            d:	'Greater freedom',
+            
+            
+            correct: 'c'
+        },
+        {
+            question: '13. Which of the following motivators is the most basic need in Maslows hierarchy',
+            a:	'Physiological',
+            b:	'Safety',
+            c:	'Belonging',
+            d:	'Esteem',
+            
+            
+            correct: 'a'
+        },
+        {
+            question: '14. Which one of the following is a feature of McGregors Theory X?',
+            a:	'Workers enjoy responsibility.',
+            b:	'Workers like to be directed.',
+            c:	'Workers view physical and mental effort as a natural part of work.',
+            d:	'Workers like to show initiative.',
+            
+            correct: 'b'
+        },
+        {
+            question: '15. What is the richest form of information?',
+            a:	'Telephone',
+            b:	'E-mail',
+            c:	'Face-to-face',
+            d:	'Video conference',
+            
+            
+            correct: 'c'
+        },
+        {
+            question: '16. Which of these is not an advantage of a virtual team?',
+            a:	'Team member,s can be recruited for their specific skills or experience.',
+            b:	'Conflict can be resolved easily using face-to-face meetings.',
+            c:	'No overhead costs associated with physical buildings.',
+            d:	'Low transport or commuting costs.',
+            
+            
+            correct: 'b'
+        },
+           {
+            question: '17.What is a strategic alliance?',
+            a:	'Formal agreement committing two or more firms to exchange resources to produce products or services',
+            b:	'Any form of partnership between one firm and another',
+            c:	'Formal agreement to share profits from a shared investment',
+            d:	'Formal agreement to share knowledge',
+            
+            
+            correct: 'a'
+        },
+        
+        {
+            question: '18. What is not an advantage of a hierarchical structure',
+            a:'The ability to organise people',
+            b:'The power to command and direct',	
+            c:'The need for order',
+            d:'The right to change jobs',	
+            
+            correct: 'b'
+        },
+        {
+            question: '19. What is not a purpose of an organisational structure?',
+            a:	'To coordinate people and resources',
+            b:	'To organise lines of communication',
+            c:	'To limit workers rights',
+            d:	'To formalise authority',
+            
+            
+            correct: 'c'
+        },
+        {
+            question: '20. What is the term for an organisation that adapts to changes in the environment by quickly responding to shifts in supply and demand conditions?',
+            a:	'Opportunistic organisation',
+            b:	'Agile organisation',
+            c:	'Enterprising organisation',
+            d:	'International organisation',
+            
+            correct: 'b'
+        },
+        {
+            question: '21. What is the overriding philosophy underpinning the strategic direction of an organisation?',
+            a:	'Vision statement',
+            b:	'Strategy statement',
+            c:	'Mission statement',
+            d:	'Business statement',
+            
+            
+            correct: 'c'
+        },
+        {
+            question:'22. What does PEST stand for?',
+            a:	'Political, environmental, strategic, testing',
+            b:	'Political, environmental, strategic, technological',
+            c:	'Political, economic, strategic, technological',
+            d:	'Political, economic, social, technological',
+                
+            
+            correct: 'd'
+        },
+       { 
+           question:'23. When does an acquisition take place?',
+       a:	'Two unequal partners become one after a buyout.',
+       b:	'Two equal partners decide to work together.',
+       c:	'Two equal partners share resources.',
+       d:	'Two unequal partners join together.',
+             correct: 'a'
+    },
+
+
+{
+    question:'24. What is the term for the extent to which competition in one country is influenced by competition in other countries?',
+a:	'International trade',
+b:	'Global economics',
+c:	'Globalisation',
+d:	'Global competition',
+           correct: 'c'
+},
+{
+    question: '25. What is NOT part of a marketing plan?',
+a:	'Marketing mix',
+b:	'Government support',
+c:	'Demand analysis',
+d:	'Competitor analysis',
+correct : 'b'
+},
+
+{
+  question: '26 What is the term for a market that is defined by specific characteristics?',
+a:	'Market segment',
+b:	'Market niche',
+c:  'Market division',
+d:  'Market characteristic',
+correct : 'a'
+},
+
+{
+  question:`27. What do the four 'P's' of marketing relate to?`,
+a: 'Product, price, press and promotion',
+b: 'Product, price, publicity and promotion',
+c: 'Product, price, post and promotion',
+d: 'Product, price, place and promotion',
+   correct: 'd'
+},
+
+
+{
+    question:'28. What does CRM stand for?',
+a:	'Consumer Relations Management',
+b:	'Customer Relationship Marketing',
+c:	'Customer Relationship Management',
+d:	'Consumer Returns Management',
+correct: 'c'
+},
+
+{ 
+    question:'29. Why is an innovative spirit harder to have in a larger company?',
+a:	'Large firms are more analytical and careful as they mature.',
+b:	'Large firms encourage innovation out of the Research and Development departments only.',
+c:	'Large firms do not reward innovation.',
+d:	'Large firms keep employees so busy there is little time left for innovation.',
+    correct:'a' 
+},
+
+
+
+{ 
+   question: '30. Consumer ________ arise(s) from a gap between the consumers actual state and their ideal state.', 
+a:	'cognition',
+b:	'needs',
+c:	'utility',
+d:	'drive',
+correct: 'b'
+},
+
+
+{
+    question: `31. How do wants differ from needs?`,
+a:	`Wants are independent of culture`,
+b:	`Wants are more specific`,
+c:	`Wants create demand for products`,
+d:	`Wants are independent of a person's experiences`,
+correct: 'b'
+},
+
+
+
+{
+    question:`32. The study of the statistical characteristics of a population is called ________.`,
+a:	`psychographics`,
+b:	`demographics`,
+c:	`behavioral segmentation`,
+d:	`anthropology`,
+correct: 'b'
+},
+
+
+{ 
+    question:'33. A company who ignores differences among buyers and offers only one product line presented with the same communication, and distribution strategies most likely uses which of the following targeting strategies?',
+a:	'cause-related marketing',
+b:	'mass marketing',
+c:	'individualized marketing',
+d:	'micromarketing',
+correct: 'b' 
+},
+
+{ question: '34. ________ consists of the four key elements of marketing strategy: product, price, distribution, and customer communication.', 
+a:	'Positioning',
+b:	'Social commerce',
+c:	'Market development',
+d:	'Marketing mix',
+correct: 'd'
+},
+
+{
+    question: '35. The value captured from customers in exchange for the value offered in a product determines ________.',
+a:	'marketing mix',
+b:	'product',
+c:	'price',
+d:	'promotion',
+correct: 'c'
+},
+
+
+
+{
+    question:'36. Which of the following refers to a graphical representation of a brand?',
+a:	'logo',
+b:	'brand equity',
+c:	'brand name',
+d:	'license',
+correct: 'a' 
+},
+
+
+{ question: '37. Price elasticity is a measure of the sensitivity of ________ to changes in price.',
+a:	'variable cost',
+b:	'demand',
+c:	'income',
+d:	'yield',
+correct:'b'
+},
+
+
+
+
+{ question: ' 38. Which of the following is NOT considered a type of reseller?',
+a:	'Manufacturer',
+b:	'Wholesaler',
+c:	'Retailer',
+d:	'Distributor',
+correct:'a'
+},
+
+{
+    question: `39. The promotion “P” of marketing is also known as ________.`,
+a:	'Product Differentiation',
+b:	'Marketing Communication',
+c:	'Distribution',
+d:	'Cost',
+correct: 'b'
+},
+
+
+{
+    question: '40. Anything that can be offered to a market for attention, acquisition, use, or consumption that might satisfy a want or need is called a(n):',
+a:	'idea',
+b:	'demand',
+c:	'product',
+d:	'service',
+correct:'c'
+},
+
+
+{
+    question: '41. The extended Ps of service marketing mix are:',
+a:	'People, Product, Place',
+b:	'Price Physical Evidence, Promotion',
+c:	'Product, Process, Physical Environment',
+d:	'Physical Environment, Process, People',
+correct: 'd' 
+},
+
+
+{ 
+    question: '42. One traditional depiction of marketing activities is in terms of the marketing mix or four Ps. The four Ps are characterized as being ________.',
+a:	'product, positioning, place, and price',
+b:	'product, production, price, and place',
+c:	'promotion, place, positioning, and price',
+d:	'product, price, promotion, and place',
+correct: 'd'
+},
+
+
+{
+    question:'43. Which product is MOST likely to be purchased through routine decision making?',
+a:	'Television set',
+b:	'Soft drink',
+c:	'Shirt',
+d:	'Car',
+correct: 'b'
+},
+
+
+
+{
+ question: '44. In order for exchange to occur:', 
+a:	'a complex societal system must be involved',
+b:	'organized marketing activities must also occur',
+c:	'a profit-oriented organization must be involved',
+d:	'each party must have something of value to the other party',
+correct:'d'
+},
+
+{
+    question: '45. Need becomes ________ when they are directed towards a specific object. ',
+a:	'Actual need',
+b:	'Satisfaction',
+c:	'Want',
+d:	'Demand',
+correct:'c'
+},
+
+{
+    question: ' 46. Which one of the following BEST describes the human need?',
+a:	'French-fries',
+b:	'Food',
+c:	'Burger',
+d:	'Pizza',
+
+correct:'b'
+},
+
+{
+    question:'47. ________ is money that an organization brings in through the sale of its goods and services.',
+a:	'Revenue',
+b:	'Competitive advantage',
+c:	'Sales',
+d:	'Capital investment',
+correct:'a'},
+
+{
+    question:'48. The risk and reward relationship ________.',
+a:	'is a key aspect of social responsibility ',
+b:	'applies only to service-intensive businesses',
+c:	'is fundamental to businesses in every modern economy',
+d:	'is essential to the counter-cyclical business model',
+correct:'c'
+},
+
+
+{
+    question:'49. ________ is the term used to describe the difference between revenue and expenses.',
+a:	'Profit',
+b:	'Revenue',
+c:	'Interest',
+d:	'Sales',
+correct:'a'
+},
+
+
+{
+    question:'50. ________ refers to some aspect of a product or company that makes it more appealing to target customers.',
+a:	'Economies of scale ',
+b:	'Horizontal integration ',
+c:	'Competitive advantage',
+d:	'Vertical integration', 
+correct: 'c'}
+    ];
+    const quiz = document.getElementById('quiz');
+    const answerEls = document.querySelectorAll('.answer');
+    const questionEl = document.getElementById('question');
+    const a_text = document.getElementById('a_text');
+    const c_text = document.getElementById('b_text');
+    const b_text = document.getElementById('c_text');
+    const d_text = document.getElementById('d_text')
+    const sumbitBTN = document.getElementById('sumbit')
+    let currentQuiz = 0;
+    let score =  0;
+    
+    loadQuiz();
+
+    function loadQuiz() {
+        deselectAnswer();
+        
+        const currentQuizData = quizData[currentQuiz];
+
+        questionEl.innerText = currentQuizData.question;
+        a_text.innerText = currentQuizData.a;
+        b_text.innerText = currentQuizData.b;
+        c_text.innerText = currentQuizData.c;
+        d_text.innerText = currentQuizData.d;
+
+
+    }
+
+    function getSelected() {
+    
+     
+        let answer = undefined;
+        
+        answerEls.forEach((answerEl) => {
+            if (answerEl.checked) {
+                answer = answerEl.id;
+            }
+        });
+        return answer;
+    }
+    function deselectAnswer() {
+        answerEls.forEach((answerEl) => {
+            answerEl.checked = false;
+        });
+    }
+
+    sumbitBTN.addEventListener('click', () => {
+
+        const answer = getSelected();
+            // console.log(answer);
+        if (answer) {
+            if (answer === quizData[currentQuiz].correct) {
+                score++;
+            }
+            currentQuiz++;
+            if (currentQuiz < quizData.length) {
+                loadQuiz();
+
+            } else {
+                quiz.innerHTML = `<h2> You answered correctly at ${score}/${quizData.length} questions.</2>
+             
+                <button  onClick = "location.reload()"> Reload </button>`;
+            }
+        }
+    });
